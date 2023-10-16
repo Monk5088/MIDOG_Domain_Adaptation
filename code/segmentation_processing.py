c84084f9b3e7858df788476be9ac4e11ada72241
Monk5088 <52120096+Monk5088@users.noreply.github.com>
2023-10-16 23:49:52 +0530

    Update segmentation_processing.py

--- code/segmentation_processing.py
+++ code/segmentation_processing.py
@@ -147,12 +147,12 @@ class MIDOG_Images(Dataset):
         self.mask_end_path=mask_end_path
         
         if image_dir == None:
-          self.image_dir = "/drive/MyDrive/MIDOG_Challenge_JJB/image_crops/"
+          self.image_dir = "/content/drive/MyDrive/MIDOG_Challenge_JJB/image_crops/"
         else:
           self.image_dir = image_dir
           
         if mask_dir == None:
-          self.mask_dir = "/drive/MyDrive/MIDOG_Challenge_JJB/mask_crops/"
+          self.mask_dir = "/content/drive/MyDrive/MIDOG_Challenge_JJB/mask_crops/"
         else:
           self.mask_dir = mask_dir
         
